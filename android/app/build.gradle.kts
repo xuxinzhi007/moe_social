fc40bcace97289348695653bc39e6972b4a99869
xinzhi.xu <2792632063@qq.com>
2026-01-22 19:38:30 +0800

    build(android): 更新NDK版本并启用核心库反混淆
    修改android/app/build.gradle.kts文件：
    1. 指定NDK版本为27.0.12077973
    2. 启用Java 17核心库反混淆功能
    3. 添加desugar_jdk_libs依赖
    
    同时更新pubspec.lock中所有依赖的下载源为国内镜像

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,11 +8,12 @@ plugins {
 android {
     namespace = "com.example.moe_social"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -41,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
